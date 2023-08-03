b7685db59aa2040543bacbb7d2a5180a2beca174
abhirajakhare <abhirajakhare@gmail.com>
2023-08-03 15:11:01 -0500

    [Problem 619. Biggest Single Number]

---
+++ BiggestSingleNumber.sql
@@ -0,0 +1,49 @@
+/*619. Biggest Single Number*/
+
+/* Write your PL/SQL query statement below */
+select max(t.num ) as NUM 
+from 
+(select num , count(*) from MyNumbers
+group by num having count(*)=1 ) t
+
+/*
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+Each row of this table contains an integer.
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+Find the largest single number. If there is no single number, report null.
+
+The result format is in the following example.
+
+Input: 
+MyNumbers table:
++-----+
+| num |
++-----+
+| 8   |
+| 8   |
+| 3   |
+| 3   |
+| 1   |
+| 4   |
+| 5   |
+| 6   |
++-----+
+Output: 
++-----+
+| num |
++-----+
+| 6   |
++-----+
+Explanation: The single numbers are 1, 4, 5, and 6.
+Since 6 is the largest single number, we return it.
+
+*/
+
